b3fd066a2c52575a08961733cf652dea7299f1b7
justinfei9 <jpf99@yahoo.com>
2024-05-18 17:35:52 -0400

    trying to fix eslint mjs file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import pluginJs from "@eslint/eslint-plugin";
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], parserOptions: { sourceType: "commonjs" }},
+  { globals: globals.browser },
   pluginJs.configs.recommended,
 ];
